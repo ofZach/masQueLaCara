3178cdf0e62b41451c24efa2a675f5b7f3ab5009
gordey <kewava@gmail.com>
2016-02-05 12:28:09 -0500

    folders

--- javascript/pointsTest/src/Masks/SvgMasks.js
+++ javascript/pointsTest/src/Masks/SvgMasks.js
@@ -6,6 +6,10 @@ class PyramidMask extends MaskBase {
 			eyeL: new SvgLoader(),
 			eyeR: new SvgLoader(),
 		}
+		this.offsetPos = {
+			eyeL: [0, 0],
+			eyeR: [0, 0],
+		}
 		this.faceParts['eyeL'].setup('eyeL','assets/StrokeFaceEye.svg');
 		this.faceParts['eyeR'].setup('eyeR','assets/StrokeFaceEye.svg');
 	}
@@ -13,12 +17,25 @@ class PyramidMask extends MaskBase {
 		this.faceParts['eyeL'].setPosition(data['eyeLPos']);
 		this.faceParts['eyeR'].setPosition(data['eyeRPos']);
 	}
-	clearParameters(settings){
-
-	}
 	addParameters(settings){
-
+		this.addParametersXY(settings, this.offsetPos, 0, 100);
 	}
+	clearParameters(settings){
+		this.removeParametersXY(settings, this.offsetPos);
+	}
+	addParametersXY(settings, param , min, max){
+		for(var prop in param){
+			settings.addRange(prop +" X", min, max, param[prop][0], 1, param[prop][0]);
+			settings.addRange(prop +" Y", min, max, param[prop][1], 1, param[prop][1]);
+		}
+	}
+	removeParametersXY(settings, param){
+		for(var prop in param){
+			settings.removeControl(prop +" X");
+			settings.removeControl(prop +" Y");
+		}
+	}
+
 }
 class CircleMask extends MaskBase {
 	setup(){
@@ -34,10 +51,10 @@ class CircleMask extends MaskBase {
 		this.faceParts['eyeL'].setPosition(data['eyeLPos']);
 		this.faceParts['eyeR'].setPosition(data['eyeRPos']);
 	}
-	clearParameters(settings){
-
-	}
 	addParameters(settings){
 		
+	}
+	clearParameters(settings){
+
 	}
 }
