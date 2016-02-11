8ac458cea3f976f8cdaf55d4f97801799966dc79
gordey <kewava@gmail.com>
2016-02-11 13:15:15 -0500

    group chain improved

--- javascript/paperShapeTest/src/GroupChain.js
+++ javascript/paperShapeTest/src/GroupChain.js
@@ -1,11 +1,14 @@
 var point = new Point(0, 0);
-var size = new Size(60, 60);
+var size = new Size(70, 60);
 
-var rect1 = new Shape.Rectangle(point, size);
+var rect1 = new Shape.Rectangle(point, [90, 100]);
 rect1.strokeColor = 'white';
 
-var rect2 = new Shape.Rectangle(point, size);
+var rect2 = new Shape.Rectangle(point, [20,200]);
 rect2.strokeColor = 'red';
+
+var rect3 = new Shape.Rectangle(point, size);
+rect3.strokeColor = 'green';
 
 var group1 = new Group();
 group1.addChild(rect1);
@@ -14,15 +17,27 @@ group1.transformContent = false;
 var group2 = new Group();
 group2.addChild(rect2);
 group2.transformContent = false;
-console.log("group1.bounds = " + group1.bounds);
-group2.pivot = [0, 0];
 
+var group3 = new Group();
+group3.addChild(rect3);
+group3.transformContent = false;
+
+group3.pivot = new Point(0, group3.bounds.height/2);
+group3.position = new Point(group2.bounds.width, group2.bounds.height/2);
+
+group2.pivot = new Point(0, group2.bounds.height/2);
+group2.position = new Point(group1.bounds.width, group1.bounds.height/2);
+
+group1.pivot = new Point(0, rect1.bounds.height/2);
+group1.position = new Point(250, 250);
+group2.addChild(group3);
 group1.addChild(group2);
+
 var frame = 0;
 function onFrame () {
 	frame ++;
-	group2.rotation = frame;
-	group1.position
+	group2.rotation = Math.cos(frame/9)*20;
+	group3.rotation = Math.cos(frame/9)*20;
 }
 var mouse = new Point(0, 0);
 function onMouseMove(event) {
